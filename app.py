00f4b9cfa0fb220323c4f8bdf81b043900fe9ea0
Amartya Pandey <pandeyamartya@outlook.com>
2024-12-05 10:37:55 +0530

    Submission commit

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ from pptx import Presentation
 
 app = Flask(__name__)
 
+import warnings
+from sklearn.exceptions import InconsistentVersionWarning
+warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
+
 # Load the model and vectorizer
 with open('model.dill', 'rb') as model_file:
     model = dill.load(model_file)
